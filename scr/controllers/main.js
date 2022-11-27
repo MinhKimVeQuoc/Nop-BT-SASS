a5e78ebca4bb2951d230ad7c8e38a286ba4a62bb
MinhKimVeQuoc <minh27121297@gmail.com>
2022-11-27 09:36:20 +0700

    Em làm không kịp em nộp trước ạ ^^

---
+++ scr/controllers/main.js
@@ -0,0 +1,67 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+function getEle(id) {
+  return document.getElementById(id)
+}
+
+const body = getEle('body');
+const header = getEle('header');
+const compare = header.offsetTop;
+
+
+// console.log(body.offsetTop);
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+
+  var timer = null;
+  window.addEventListener('scroll', function () {
+    // if (window.scrollY <= 225) {
+    //   getEle("header").style.position = 'fixed';
+    //   getEle("header").classList.remove('header-fixed')
+    //   getEle("header").style.top = "0"
+    // }
+   
+
+        if (window.scrollY <= 100) {
+          getEle("header").style.position = 'absolute';
+          getEle("header").classList.remove('header-fixed')
+          getEle("header").style.top = "0";
+        }
+
+        if (window.scrollY >= 225) {
+          getEle("header").style.position = 'fixed';
+          getEle("header").style.top = "-89px";
+        }
+
+        if (window.scrollY >= 228) {
+          getEle("header").classList.add('header-fixed')
+          if (timer != null) {
+            this.clearTimeout(timer);
+            // timer khác rỗng 
+
+            getEle("header").style.top = "-89px";
+          }
+          timer = this.setTimeout(function () {
+            getEle("header").style.top = "0";
+
+          }, 500)
+        }
+    
+
+  })
+
+  // xem để biết thêm
+  // console.log('documentElement', document.documentElement.scrollTop);
+  // console.log('body', document.body.scrollTop)
+}
